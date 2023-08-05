06af55f0b1128f94b88bdee12c2aef8be2f69130
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-08-05 14:50:11 +0300

    Script Animations Update

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    private Animator anim;
+    private Movement pm;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        pm = GetComponent<Movement>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("X", pm.DireccionMovimiento.x);
+        anim.SetFloat("Y", pm.DireccionMovimiento.y);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,15 +11,11 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    public Vector2 DireccionMovimiento => directionMove;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
     }
 
     // Update is called once per frame
