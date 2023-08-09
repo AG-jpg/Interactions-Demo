babfc6c9d88d8b4550b36362bef7c4732f5e074b
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-08-09 07:17:38 +0300

    Animations Layer Control

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Animations : MonoBehaviour
 {
+    [SerializeField] private string layerIdle;
+    [SerializeField] private string layerWalk;
+
     private Animator anim;
     private Movement pm;
 
@@ -20,10 +23,34 @@ public class Animations : MonoBehaviour
 
     void Update()
     {
+        UpdateLayer();
+        
         if (pm.Moving)
         {
             anim.SetFloat("X", pm.DireccionMovimiento.x);
             anim.SetFloat("Y", pm.DireccionMovimiento.y);
+        }
+    }
+
+    private void ActiveLayer(string layerName)
+    {
+        for (int i = 0; i < anim.layerCount; i++)
+        {
+            anim.SetLayerWeight(i, 0);
+        }
+
+        anim.SetLayerWeight(anim.GetLayerIndex(layerName), 1);
+    }
+
+    private void UpdateLayer()
+    {
+        if(pm.Moving)
+        {
+            ActiveLayer(layerWalk);
+        }
+        else
+        {
+            ActiveLayer(layerIdle);
         }
     }
 }
