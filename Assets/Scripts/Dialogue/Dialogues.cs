d702d7a4a0162c00fd2fcb117824063991fc38ca
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-08-19 07:24:36 +0300

    DIalogue Folder Created

---
+++ Assets/Scripts/Dialogue/Dialogues.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogues : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    public GameObject ziman;
+    private GameObject player;
+    private BoxCollider2D bc;
+    public bool IsPaused;
+    public bool chat;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        bc = GetComponent<BoxCollider2D>();
+        ziman.SetActive(true);
+        IsPaused = false;
+        chat = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            chat = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (IsPaused)
+        {
+            Time.timeScale = 0;
+            TakeAction();
+        }
+
+        if(chat && Input.GetKeyDown(KeyCode.Return))
+        {
+            Time.timeScale = 0;
+            Talk();
+        }
+    }
+
+    public void Talk()
+    {
+        dialogueBox.SetActive(true);
+        IsPaused = true;
+    }
+
+    private void TakeAction()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Time.timeScale = 1;
+            IsPaused = false;
+            chat = false;
+            dialogueBox.SetActive(false);
+            ziman.SetActive(false);
+            Destroy(bc);
+        }
+    }
+}
