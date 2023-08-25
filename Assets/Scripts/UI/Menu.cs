482ab3c7241a34a10b756e98ab2b30c0a9c559fb
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-08-25 16:21:24 +0300

    Menu Script Added

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject bg;
+    public static bool gameIsPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+}
