de080eec28653e6dfaa74f8fd7e1d9361b4ac2eb
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-08-30 07:10:35 +0300

    UI Manager Script Updated

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,22 +10,27 @@ public class UIManager : Singleton<UIManager>
     [SerializeField] private Image expPlayer;
 
     [Header("Texto")] 
-    [SerializeField] private Text expTMP;
+    [SerializeField] private TextMeshProUGUI expTMP;
 
     private float expActual;
     private float expRequiredNewLevel;
 
-    private void UpdatePlayer()
+    private void Update()
     {
-        expPlayer.fillAmount = Mathf.Lerp(expPlayer.fillAmount,
+        UpdateUIPlayer();
+    }
+
+    private void UpdateUIPlayer()
+    {
+        expPlayer.fillAmount = Mathf.Lerp(expPlayer.fillAmount, 
         expActual / expRequiredNewLevel, 10f * Time.deltaTime);
 
         expTMP.text = $"{expActual}/{expRequiredNewLevel}";
     }
 
-    public void UpdateExpPlayer(float pExpActual, float pExpReq)
+    public void UpdateExpPlayer(float pExpActual, float pExpRequerida)
     {
         expActual = pExpActual;
-        expRequiredNewLevel = pExpReq;
+        expRequiredNewLevel = pExpRequerida;
     }
 }
