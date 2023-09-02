dbb51e11a597c2831cef4b521f11de1dab905e80
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-09-02 08:26:36 +0300

    Player Stats Editor Added

---
+++ Assets/Scripts/Player/Editor/PlayerStatsEditor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Stats))]
+public class PlayerStatsEditor : Editor
+{
+    public Stats StatsTarget => as Stats;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Reset Values"))
+        {
+            
+        }
+        
+    }
+}
