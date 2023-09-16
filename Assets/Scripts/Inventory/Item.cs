6201e59f0e5731a932b1e04b60017632a9aa0bb7
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-09-16 07:44:56 +0300

    Item Script Updated

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -2,8 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Item")]
+public enum TiposItem
+{
+    Hardware, Food, Money, Usables
+}
+
 public class Item : ScriptableObject
 {
-    
+    [Header("Parameter")]
+    public string ID;
+    public string Name;
+    public Sprite Icon;
+    [TextArea] public string Description;
+
+    [Header("Information")]
+    public TiposItem Tipo;
+    public bool Consumible;
+    public bool Acumulable;
+    public int AcumulacionMax;
+
+    [HideInInspector]    
+    public int Cantidad;
 }
