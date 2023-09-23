26d7cc85b6af3d7cd47efc06216c5ed094cdb404
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-09-23 10:44:00 +0300

    Inventory Update

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -15,4 +15,26 @@ public class Inventory : Singleton<Inventory>
         itemsInventario = new Item[numeroSlots];
     }
 
+    private void AddItem(Item itemtoAdd,int Cantidad)
+    {
+        if(itemtoAdd == null)
+        {
+            return;
+        }
+    }
+
+    private List<int> VerificarExistencias(string itemID)
+    {
+        List<int> indexItem = new List<int>();
+        for (int i=0; i < itemsInventario.Length; i++)
+        {
+            if (itemsInventario[i].ID == itemID)
+            {
+                indexItem.Add(i);
+            }
+        }
+
+        return indexItem;
+    }
+
 }
