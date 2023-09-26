b87e959ae6a77bdd1d5bbfa22734a84c78ec5c22
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-09-26 14:32:38 +0300

    Player and Enegy Scripts Updated

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Stats stats;
+    public Energy Energy { get; private set; }
+
+
+    private void Awake()
+    {
+        Energy = GetComponent<Energy>();
+    }
+
     private void AtributoRespuesta(AttributeType type)
     {
         if (stats.PuntosDisponibles <= 0)
