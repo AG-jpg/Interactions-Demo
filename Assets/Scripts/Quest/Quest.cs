17f2797de303a62ed29e74ad19614ea4eadef248
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-09-30 09:33:29 +0300

    Quest Scripts Added

---
+++ Assets/Scripts/Quest/Quest.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Quest : ScriptableObject
+{
+    [Header ("Info")]
+    public string Name;
+    public string ID;
+    public int CantidadObjetivo;
+
+    [Header("Description")]
+    public string Description;
+
+    [Header("Rewards")]
+    public int Credits;
+    public float Experience;
+
+    [HideInInspector] public int cantidadActual;
+    [HideInInspector] bool QuestCompleted;
+
+}
+
+[Serializable]
+public class QuestRewardItem
+{
+    public Item Item;
+    public int cantidad;
+}
