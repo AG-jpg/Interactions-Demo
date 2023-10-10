5bcc7ee3a6ae9696e6b4b77cdf24d0889d5e051f
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-10-10 17:16:16 +0300

    Quest Update to OnEnable()

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,65 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Quest : ScriptableObject
+{
+    public static Action<Quest> EventQuestCompleted;
+
+    [Header ("Info")]
+    public string Name;
+    public string ID;
+    public int CantidadObjetivo;
+
+    [Header("Description")]
+    public string Description;
+
+    [Header("Rewards")]
+    public int Credits;
+    public float Experience;
+    public QuestRewardItem RewardItem;
+
+    [HideInInspector] public int cantidadActual;
+    [HideInInspector] public bool QuestCompletedCheck;
+
+    public void AddProgress(int cantidad)
+    {
+        cantidadActual += cantidad;
+        VerifyCompletedQuest();
+    }
+
+    private void VerifyCompletedQuest()
+    {
+        if(cantidadActual >= CantidadObjetivo)
+        {
+            cantidadActual = CantidadObjetivo;
+            CompletedQuest();
+        }
+    }
+
+    private void CompletedQuest()
+    {
+        if(QuestCompletedCheck)
+        {
+            return;
+        }
+
+        QuestCompletedCheck = true;
+        EventQuestCompleted?.Invoke(this);
+    }
+
+    private void OnEnable()
+    {
+        QuestCompletedCheck = false;
+        cantidadActual = 0;
+    }
+
+}
+
+[Serializable]
+public class QuestRewardItem
+{ 
+    public Item Item;
+    public int cantidad;
+}
