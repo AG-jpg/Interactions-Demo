3ff33737722f177bf70c08e3f753383588e6d8dc
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-10-23 23:36:47 +0300

    Quest Container Added

---
+++ Assets/Scripts/Quest/QuestContainer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestContainer : MonoBehaviour
+{
+    [Header("Info")]
+    public string Name;
+
+    [Header("Quests")]
+    [SerializeField] public Quest[] Quests;
+
+    public bool NPCActive;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+                NPCActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            NPCActive = false;
+        }
+    }
+}
