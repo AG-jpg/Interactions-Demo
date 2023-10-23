5d7e3db0d74b63e1ce0b064a37e4c7e00cf7ae27
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-10-23 23:28:26 +0300

    Quest Manager Updated

--- Assets/Scripts/Quest/QuestManager.cs
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -1,41 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class QuestManager : Singleton<QuestManager>
 {
-    [Header ("Quests")]
+    [Header("Quests")]
     [SerializeField] private Quest[] questDisponibles;
 
-    [Header ("Inspector Quest Quests")]
+    [Header("Inspector Quests")]
     [SerializeField] private InspectorQuest inspectorQuestPrefab;
     [SerializeField] private Transform InpsectorQuestContainer;
 
-    [Header ("Player Quests")]
+    [Header("Player Quests")]
     [SerializeField] private PlayerQuest playerQuestPrefab;
 
     [SerializeField] private Transform playerQuestContainer;
 
+    [Header("NPC Quests")]
+    [SerializeField] private QuestContainer[] NPCquest;
+
     void Start()
     {
+        //questDisponibles = new Quest[questDisponibles.Length + 1];
+        LoadNPCQuest();
         LoadQuestInspector();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.V))
+        LoadNPCQuest();
+        LoadQuestInspector();
+
+        if (Input.GetKeyDown(KeyCode.V))
         {
             AddProgress("Rock the Casbah", 1);
-            //AddProgress("Looking for Water", 1);
         }
     }
 
     private void LoadQuestInspector()
     {
-        for(int i=0; i < questDisponibles.Length; i++)
+        for (int i = 0; i < questDisponibles.Length; i++)
         {
             QuestDescription newQuest = Instantiate(inspectorQuestPrefab, InpsectorQuestContainer);
             newQuest.ConfigureQuestUI(questDisponibles[i]);
+        }
+    }
+
+    private void LoadNPCQuest()
+    {
+        for (int i = 0; i < NPCquest.Length; i++)
+        {
+            if (NPCquest[i] != null)
+            {
+                if (NPCquest[i].NPCActive == true)
+                {
+                    questDisponibles = NPCquest[i].Quests;
+                }
+            }
+
         }
     }
 
@@ -60,7 +84,8 @@ public class QuestManager : Singleton<QuestManager>
     {
         for (int i = 0; i < questDisponibles.Length; i++)
         {
-            if(questDisponibles[i].ID == questID)
+            //Aquí hay que editar la lista
+            if (questDisponibles[i].ID == questID)
             {
                 return questDisponibles[i];
             }
