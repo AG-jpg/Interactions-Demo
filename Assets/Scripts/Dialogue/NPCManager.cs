67d33b3c6732e677ddb45f7e99612578237b0b2e
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-11-08 19:28:44 +0200

    Guard Management Added

--- Assets/Scripts/Dialogue/NPCManager.cs
+++ Assets/Scripts/Dialogue/NPCManager.cs
@@ -8,9 +8,29 @@ public class NPCManager : MonoBehaviour
     [SerializeField] public GameObject Ella01;
     [SerializeField] public GameObject Ella02;
 
+    [Header("Johny")]
+    [SerializeField] public GameObject Johny01;
+    [SerializeField] public GameObject Johny02;
+
+    [Header("Guard")]
+    [SerializeField] public GameObject Guard01;
+    [SerializeField] public GameObject Guard02;
+
     public void HideElla()
     {
         Ella01.SetActive(false);
         Ella02.SetActive(true);
+    }
+
+    public void HideJohny()
+    {
+        Johny01.SetActive(false);
+        Johny02.SetActive(true);
+    }
+
+    public void HideGuard()
+    {
+        Guard01.SetActive(false);
+        Guard02.SetActive(true);
     }
 }
