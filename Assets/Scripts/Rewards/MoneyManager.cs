c37ec2f72f9df9fc85f5e288043fa2e488a1eef2
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-11-28 16:39:20 +0200

    Rewards Folder Added

---
+++ Assets/Scripts/Rewards/MoneyManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : Singleton<MoneyManager>
+{
+    public int TotalCredits { get; set; }
+    private string KEY_CREDITS = "MYGAME_CREDITS";
+
+    public void AddCredits(int cantidad)
+    {
+        TotalCredits += cantidad;
+        PlayerPrefs.SetInt(KEY_CREDITS, TotalCredits);
+        PlayerPrefs.Save();
+    }
+}
