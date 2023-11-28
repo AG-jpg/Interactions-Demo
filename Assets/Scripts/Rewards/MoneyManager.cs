29ae13be2efeb31631b17ff75014e82b590b6eaa
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-11-28 16:44:33 +0200

    MoneyManager Updated

--- Assets/Scripts/Rewards/MoneyManager.cs
+++ Assets/Scripts/Rewards/MoneyManager.cs
@@ -7,9 +7,31 @@ public class MoneyManager : Singleton<MoneyManager>
     public int TotalCredits { get; set; }
     private string KEY_CREDITS = "MYGAME_CREDITS";
 
+    private void Start()
+    {
+        
+    }
+
+    private void LoadCredits()
+    {
+        TotalCredits = PlayerPrefs.GetInt(KEY_CREDITS);
+    }
+
     public void AddCredits(int cantidad)
     {
         TotalCredits += cantidad;
+        PlayerPrefs.SetInt(KEY_CREDITS, TotalCredits);
+        PlayerPrefs.Save();
+    }
+
+    public void RemoveCredits(int cantidad)
+    {
+        if(cantidad > TotalCredits)
+        {
+            return;
+        }
+
+        TotalCredits -= cantidad;
         PlayerPrefs.SetInt(KEY_CREDITS, TotalCredits);
         PlayerPrefs.Save();
     }
