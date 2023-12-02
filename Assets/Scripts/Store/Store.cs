de1683b25b0834a2020fc4f8c43f87518e724e04
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-12-02 06:50:33 +0200

    Store Script Ready

--- Assets/Scripts/Store/Store.cs
+++ Assets/Scripts/Store/Store.cs
@@ -6,10 +6,20 @@ using UnityEngine.UI;
 
 public class Store : MonoBehaviour
 {
-   [Header "Configuration"]
-   [SerializeField] private Image icon;
-   [SerializeField] private TextMeshProUGUI itemName;
-   [SerializeField] private TextMeshProUGUI itemCost;
-   [SerializeField] private TextMeshProUGUI cantidad;
+    [Header("Item Info")]
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private TextMeshProUGUI itemCost;
+    [SerializeField] private TextMeshProUGUI cantidad;
+
+    public ItemVenta itemLoaded { get; set; }
+
+    public void ConfigureItemSale(ItemVenta itemVenta)
+    {
+        itemLoaded = itemVenta;
+        icon.sprite = itemVenta.Item.Icon;
+        itemName.text = itemVenta.Item.Name;
+        itemCost.text = itemVenta.Cost.ToString();
+    }
 
 }
