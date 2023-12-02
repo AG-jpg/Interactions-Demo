6530e3b28c1656bf1b42aa7eb66b7026aeb783a0
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-12-02 06:42:55 +0200

    Add files via upload

---
+++ Assets/Scripts/Store/ItemVenta.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ItemVenta
+{
+    public string Name;
+    public Item Item;
+    public int Cost;
+}
