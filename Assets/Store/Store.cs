74514cc25da5592ec6442c0e24b5cb4342f990a5
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-12-02 12:12:27 +0200

    AddItem Added to Store Item

--- Assets/Store/Store.cs
+++ Assets/Store/Store.cs
@@ -6,15 +6,42 @@ using UnityEngine.UI;
 
 public class Store : MonoBehaviour
 {
-   [Header("Item Info")]
-   [SerializeField] private Image icon;
-   [SerializeField] private TextMeshProUGUI itemName;
-   [SerializeField] private TextMeshProUGUI itemCost;
-   [SerializeField] private TextMeshProUGUI cantidad;
+    [Header("Item Info")]
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private TextMeshProUGUI itemCost;
+    [SerializeField] private TextMeshProUGUI cantidad;
 
-   public void ConfigureItemSale(ItemVenta itemVenta)
-   {
+    public ItemVenta itemLoaded { get; set; }
 
-   }
+    private int cantidadCompra;
+    private int initialCost;
+    private int actualCost;
+    private void Update()
+    {
+        cantidad.text = cantidadCompra.ToString();
+        itemCost.text = actualCost.ToString();
+    }
+
+    public void ConfigureItemSale(ItemVenta itemVenta)
+    {
+        itemLoaded = itemVenta;
+        icon.sprite = itemVenta.Item.Icon;
+        itemName.text = itemVenta.Item.Name;
+        itemCost.text = itemVenta.Cost.ToString();
+        cantidadCompra = 1;
+        initialCost = itemVenta.Cost;
+        actualCost = itemVenta.Cost;
+    }
+
+    public void AddItemtoBuy()
+    {
+        int purchaseCost = initialCost * (cantidadCompra + 1);
+        if(MoneyManager.Instance.TotalCredits >= purchaseCost)
+        {
+            cantidadCompra++;
+            actualCost = initialCost * cantidadCompra;
+        }
+    }
 
 }
