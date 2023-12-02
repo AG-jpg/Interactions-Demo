a5049472639182f0ddfa732ba7e68867dc3828d9
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-12-02 12:01:13 +0200

    Store Item Text Updated

--- Assets/Scripts/Store/Store.cs
+++ Assets/Scripts/Store/Store.cs
@@ -14,12 +14,24 @@ public class Store : MonoBehaviour
 
     public ItemVenta itemLoaded { get; set; }
 
+    private int cantidadCompra;
+    private int initialCost;
+    private int actualCost;
+    private void Update()
+    {
+        cantidad.text = cantidadCompra.ToString();
+        itemCost.text = actualCost.ToString();
+    }
+
     public void ConfigureItemSale(ItemVenta itemVenta)
     {
         itemLoaded = itemVenta;
         icon.sprite = itemVenta.Item.Icon;
         itemName.text = itemVenta.Item.Name;
         itemCost.text = itemVenta.Cost.ToString();
+        cantidadCompra = 1;
+        initialCost = itemVenta.Cost;
+        actualCost = itemVenta.Cost;
     }
 
 }
