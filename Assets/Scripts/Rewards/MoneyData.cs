2c08402f97a228e02cc7d069bc5e8ba73677f4ed
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2024-01-02 14:44:44 +0200

    Money Saving System

---
+++ Assets/Scripts/Rewards/MoneyData.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class MoneyData
+{
+    public int creditsData;
+}
--- Assets/Scripts/Rewards/MoneyManager.cs
+++ Assets/Scripts/Rewards/MoneyManager.cs
@@ -1,29 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using BayatGames.SaveGameFree;
 using UnityEngine;
 
 public class MoneyManager : Singleton<MoneyManager>
 {
     [SerializeField] private int startCredits;
     public int TotalCredits { get; set; }
-    private string KEY_CREDITS = "MYGAME_CREDITS";
+    private readonly string MONEY_KEY = "Money105020";
 
     private void Start()
     {
-        //PlayerPrefs.DeleteKey(KEY_CREDITS);
-        LoadCredits();
-    }
-
-    public void LoadCredits()
-    {
-        TotalCredits = PlayerPrefs.GetInt(KEY_CREDITS, startCredits);
+        AddCredits(startCredits);
+        LoadMoney();
     }
 
     public void AddCredits(int cantidad)
     {
         TotalCredits += cantidad;
-        PlayerPrefs.SetInt(KEY_CREDITS, TotalCredits);
-        PlayerPrefs.Save();
+        SaveMoney();
     }
 
     public void RemoveCredits(int cantidad)
@@ -34,7 +29,28 @@ public class MoneyManager : Singleton<MoneyManager>
         }
 
         TotalCredits -= cantidad;
-        PlayerPrefs.SetInt(KEY_CREDITS, TotalCredits);
-        PlayerPrefs.Save();
+        SaveMoney();
     }
+
+    #region Saving
+
+    public MoneyData savedData;
+    public void SaveMoney()
+    {
+        savedData = new MoneyData();
+        savedData.creditsData = TotalCredits;
+
+        SaveGame.Save(MONEY_KEY, savedData);
+    }
+
+    public MoneyData dataLoaded;
+    public void LoadMoney()
+    {
+        if (SaveGame.Exists(MONEY_KEY))
+        {
+            dataLoaded = SaveGame.Load<MoneyData>(MONEY_KEY);
+            TotalCredits = dataLoaded.creditsData;
+        }
+    }
+    #endregion
 }
