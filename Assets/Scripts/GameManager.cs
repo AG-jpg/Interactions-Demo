5c7b005585d08d634a02886e60e6a6e80f5035cc
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2024-01-28 13:28:29 +0200

    GameManager Fixed Load Bug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,12 +17,13 @@ public class GameManager : Singleton<GameManager>
     public bool puzzleVM;
     public bool puzzleSecurity;
     public bool puzzleCage;
+    public bool puzzleBattle;
     
     [SerializeField] public GameObject battleDialogue;
 
     private void Start()
     {
-        SaveGame.DeleteAll(); //Erase Saved Data
+        //SaveGame.DeleteAll(); //Erase Saved Data
         LoadSavedGame();
     }
 
@@ -43,17 +44,16 @@ public class GameManager : Singleton<GameManager>
         {
             Player.Instance.LoadLocation();
             puzzleVM = true;
-            puzzle.easySolved = false;
+            //puzzle.easySolved = false;
             Destroy(puzzleManager);
             NPCManager.Instance.VMFInal();
             NPCManager.Instance.HideZimanGuard();
         }
         else if (puzzle.midSolved == true)
         {
-            SaveMyGame();
             Player.Instance.LoadLocation();
             puzzleSecurity = true;
-            puzzle.midSolved = false;
+            //puzzle.midSolved = false;
             Destroy(puzzleManager);
             NPCManager.Instance.HideRedDoors();
             NPCManager.Instance.FinalGuard();
@@ -64,26 +64,28 @@ public class GameManager : Singleton<GameManager>
         {
             Player.Instance.LoadLocation();
             puzzleCage = true;
-            puzzle.hardSolved = false;
+            //puzzle.hardSolved = false;
             Destroy(puzzleManager);
             NPCManager.Instance.FinalGuard();
             NPCManager.Instance.VMAfter();
             NPCManager.Instance.HideAnimals();
             NPCManager.Instance.HideKatai();
             NPCManager.Instance.DestroyMessages();
+            NPCManager.Instance.HideRedDoors();
             Destroy(Pzzl01);
             Destroy(Pzzl02);
             battleDialogue.SetActive(true);
         }
         else if (puzzle.battleFinished == true)
         {
-            SaveMyGame();
-            Destroy(puzzleManager);
             Player.Instance.LoadLocation();
-            NPCManager.Instance.FinalMission();
+            puzzleBattle = true;
+            //puzzle.battleFinished = false;
+            Destroy(puzzleManager);
             Destroy(Pzzl01);
             Destroy(Pzzl02);
-            puzzle.battleFinished = false;
+            NPCManager.Instance.FinalMission();
+            SaveMyGame();
         }
     }
 
@@ -100,7 +102,7 @@ public class GameManager : Singleton<GameManager>
 
     public void LoadSavedGame()
     {
-        Player.Instance.LoadLocation();
+        //Player.Instance.LoadLocation();
         QuestManager.Instance.LoadQuestData();
         Inventory.Instance.LoadInventory();
         Energy.Instance.LoadEnergy();
