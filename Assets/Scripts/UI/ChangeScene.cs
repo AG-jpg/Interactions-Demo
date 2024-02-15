be29df505ebddfbbfbfd77e9dfbd68fc572476b0
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2024-02-15 23:10:46 +0200

    Change Scene Updated

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public int sceneID;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SoundManager.Instance.ButtonSound();
+            SceneManager.LoadScene(sceneID);
+        }
+    }
+
     public void PressButton(int sceneID)
     {
         SoundManager.Instance.ButtonSound();
