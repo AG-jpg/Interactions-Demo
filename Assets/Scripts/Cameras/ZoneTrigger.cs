d55a1302ddcf8569a6e2f09c44f422687dc8a3ba
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2024-03-17 12:09:48 +0200

    Zone Trigger Updated

--- Assets/Scripts/Cameras/ZoneTrigger.cs
+++ Assets/Scripts/Cameras/ZoneTrigger.cs
@@ -6,10 +6,19 @@ public class ZoneTrigger : MonoBehaviour
 {
     public GameObject startpoint;
     private Player player;
+    [SerializeField] public GameObject fade;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        fade.SetActive(true);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         player.transform.position = startpoint.transform.position;
+        StartCoroutine(Fades());
+    }
+
+    private IEnumerator Fades()
+    {
+        yield return new WaitForSeconds(2f);
+        fade.SetActive(false);
     }
 }
